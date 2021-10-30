3645537da3e53a78ed667ba8ba7e7a90b4327c95
jose <josevflores911@gmail.com>
2021-10-30 12:22:01 -0300

    erase

--- entrega_final/leitorProduto.microservice/target/classes/data.sql
+++
@@ -1,9 +0,0 @@
-
-INSERT INTO produto (id, nome, taxa, descricao, preco)
-VALUES (1, 'a', 'a', 'a', 'a');
-
-INSERT INTO produto (id, nome, taxa, descricao, preco)
-VALUES (2, 'a', 'a', 'a', 'a');
-
-INSERT INTO produto (id, nome, taxa, descricao, preco)
-VALUES (3, 'a', 'a', 'a', 'a');
