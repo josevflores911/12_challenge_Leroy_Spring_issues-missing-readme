76646970182cc696c99957a63994f99fd7c6a01e
jose <josevflores911@gmail.com>
2021-11-01 01:15:04 -0300

    testes corregidos e comentados

--- leitorProduto.microservice/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-
-INSERT INTO produto (id, nome, taxa, descricao, preco)
-VALUES (3, 'a', 'a', 'a', 'a');
-
-INSERT INTO produto (id, nome, taxa, descricao, preco)
-VALUES (4, 'a', 'a', 'a', 'a');
-
-INSERT INTO produto (id, nome, taxa, descricao, preco)
-VALUES (5, 'a', 'a', 'a', 'a');
